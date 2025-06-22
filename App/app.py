712b347bd2a8b503873746f697a278ad854c9d0d
HaneenAjaz <haneenajaz10@gmail.com>
2025-06-22 14:50:54 +0530

    Update app.py

--- App/app.py
+++ App/app.py
@@ -5,7 +5,7 @@ import torch
 import torch.nn as nn
 from torchvision import transforms, models
 
-# Define your ResNet18 model architecture with 2 classes
+# Define your ResNet18 model architecture with classes
 num_classes = 2
 model = models.resnet18(pretrained=False)
 model.fc = nn.Linear(model.fc.in_features, num_classes)
